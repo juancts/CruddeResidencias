ee798b571ae6bf74fe0e67d52fa9ebdd11eca08b
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-08-12 10:31:01 +0200

    bttons added

--- app/Views/cursos/listacursos.php
+++ app/Views/cursos/listacursos.php
@@ -1,28 +1,31 @@
-<?= $header?>
-<a href="<?=base_url('crearcurso')?>">New Curs</a>
-                <table class="table table-light">
-                    <thead class="thead-light">
-                        <tr>
-                            <th>#</th>
-                            <th>Fecha</th>
-                            <th>Descripcio</th>
-                            <th>Acción</th>                       
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                            foreach($cursos as $curso):                        
-                        ?>
-                        <tr>
-                                <td><?=$curso['KEY_CURSO']?></td>
-                                <td><?=$curso['DTE_INI'];?></td>
-                                <td><?=$curso['TXT_NOMBRE']?></td>
-                                <td>Editar/
-                                    <a href="<?=base_url('borrar/'.$curso['KEY_CURSO']);?>" class="btn btn-danger" type="button">Borrar</a></td>
-                        </tr>
-                        <?php 
-                            endforeach;
-                        ?>
-                    </tbody>
-                </table>
-<?=$footer ?>    
+<?= $header ?>
+<div style="margin-top: 10px; margin-bottom: 10px; margin-right:20px; text-align: right;">
+    <a href="<?= base_url('crearcurso') ?>" class="btn btn-success">New Curs</a>
+</div>
+<table class="table table-light">
+    <thead class="thead-light">
+        <tr style="text-align: center;">
+            <th>#</th>
+            <th>Fecha</th>
+            <th>Descripcio</th>
+            <th>Acción</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($cursos as $curso) :
+        ?>
+            <tr>
+                <td><?= $curso['KEY_CURSO'] ?></td>
+                <td><?= $curso['DTE_INI']; ?></td>
+                <td><?= $curso['TXT_NOMBRE'] ?></td>
+                <td style="padding: 2px; text-align: center;"><a href="<?= base_url('editar/' . $curso['KEY_CURSO']); ?>" class="btn btn-info btn-sm m-1" type="button">Editar</a>
+                    <a href="<?= base_url('borrar/' . $curso['KEY_CURSO']); ?>" class="btn btn-danger btn-sm m-1" type="button">Borrar</a>
+                </td>
+            </tr>
+        <?php
+        endforeach;
+        ?>
+    </tbody>
+</table>
+<?= $footer ?>
--- app/Views/residencias/listaresidencias.php
+++ app/Views/residencias/listaresidencias.php
@@ -1,12 +1,14 @@
 <?= $header ?>
-<a href="<?= base_url('crear') ?>">Add Residence</a>
+<div style="margin-top: 10px; margin-bottom: 10px; margin-right:20px; text-align: right;">
+    <a href="<?= base_url('crear') ?>" class="btn btn-success">Add Residence</a>
+</div>
 <table class="table table-light">
     <thead class="thead-light">
         <tr>
             <th>#</th>
             <th>Imágen</th>
             <th>Nombre</th>
-            <th>url</th>
+            <!-- <th>url</th> -->
             <th>email</th>
             <th>telefono</th>
             <th>contacto</th>
@@ -27,11 +29,13 @@
                     <td><p class="text-center">N/A</p></td>
                 <?php } ?>
                 <td><?= $residencia['TXT_NOMBRE'] ?></td>
-                <td><?= $residencia['URL'] ?></td>
+                <!-- <td><?= $residencia['URL'] ?></td> -->
                 <td><?= $residencia['EMAIL'] ?></td>
                 <td><?= $residencia['TELEFONO'] ?></td>
                 <td><?= $residencia['CONTACTO'] ?></td>
-                <td>Editar/Borrar</td>
+                <td style="padding: 2px; text-align: center;"><a href="<?= base_url('editar/' . $residencia['KEY_RESIDENCIA']); ?>" class="btn btn-info btn-sm m-1" type="button">Editar</a>
+                    <a href="<?= base_url('borrar/' . $residencia['KEY_RESIDENCIA']); ?>" class="btn btn-danger btn-sm m-1" type="button">Borrar</a>
+                </td>
             </tr>
         <?php
         endforeach;
